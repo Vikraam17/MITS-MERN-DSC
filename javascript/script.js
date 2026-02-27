1a8752b4009cb80e13f844360a90bec944d63e87
Vikraam <vikraam17@gmail.com>
2026-02-27 16:05:42 +0530

    Js Variables, Datatypes and Operators done

---
+++ javascript/script.js
@@ -0,0 +1,74 @@
+// console.log("Hello World!!");
+// // Reinitialize and Redeclare
+// var a; //Global Scope
+// a = 10;
+// var a;
+// a = 20;
+// // Reinitialize
+// let b = 10; // Block Scope
+// // let b;
+// b = 20;
+// // Can't Redeclare or Reinitialize
+// const c = 30; //Block Scope
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+// // Datatypes
+// var num = 10; // number
+// var str = "vikraam"; // `` '' "" //string
+// var bool = true; //boolean
+// var un; //undefined
+// var nu = null; //Object (Js Bug)
+// var big = 12345678909864n; //BigInt
+// var symbol = Symbol('li'); //symbol
+
+// console.log(typeof num);
+// console.log(typeof str);
+// console.log(typeof bool);
+// console.log(typeof un);
+// console.log(typeof nu);
+// console.log(typeof big);
+// console.log(typeof symbol);
+
+// var arr = [1,2,3,4,5] //array
+// console.log(arr);
+// console.log(typeof arr);
+
+// var Obj = {
+//     name:"vikraam",
+//     dept:"DS"
+// } //Object
+// console.log(Obj); 
+// console.log(typeof Obj);
+
+// //  Arithmatic Operator
+
+// var a = 10;
+// var b ="2";
+// console.log(a+b);
+// console.log(a-b);
+// console.log(a/b);
+// console.log(a%b);
+// console.log(a*b);
+// console.log(a**b);
+
+// // Relational Operator
+
+// var a = 10;
+// var b = 20;
+// console.log(a==b);
+// console.log(a===b);
+// console.log(a!=b);
+// console.log(a!==b);
+// console.log(a>b);
+// console.log(a<b);
+// console.log(a>=b);
+// console.log(a<=b);
+
+//logical (&&,||,!)
+var a = true;
+var b = false;
+console.log(a&&b);
+console.log(a||b);
+console.log(!a)
