16040e9a9e955845a5051a1ba32d592a6bf61e63
Vikraam <vikraam17@gmail.com>
2026-03-11 14:14:06 +0530

    Auth Login and Signup created in Backend

--- Backend/server.js
+++ Backend/server.js
@@ -5,6 +5,7 @@ const app = express();
 const egRouters = require('./routers/egRouter');
 const userRouters = require('./routers/userRouter');
 const todoRouters = require('./routers/todoRouter');
+const authRouters = require('./routers/authRouter');
 const cors = require('cors');
 dotenv.config();
 connectDB();
@@ -15,6 +16,7 @@ app.use(express.json());
 app.use(egRouters);
 app.use('/api/user',userRouters);
 app.use('/api/todo',todoRouters);
+app.use('/api/auth',authRouters);
 
 const PORT = process.env.PORT;
 app.listen(PORT,()=>{
